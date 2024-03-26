298d78a4304a8762970555b08c62bee0145026eb
Gontier Julien <gontierjulien68@gmail.com>
2024-03-27 00:53:39 +0100

    Woopsie daysie
    Signed-off-by: Gontier Julien <gontierjulien68@gmail.com>

--- customize.sh
+++ customize.sh
@@ -1,7 +1,7 @@
 # Set some nice text
 ui_print ""
 ui_print "**************************************"
-ui_print "      Twemoji Remastered v14.0.3"
+ui_print "      Twemoji Remastered v15.0.3"
 ui_print "     Maintained by Julien Gontier"
 ui_print "**************************************"
 ui_print ""
